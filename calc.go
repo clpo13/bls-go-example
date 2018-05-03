5e80540eeb52dec1421736d5555c8cc02bf5b06c
Cody Logan <clpo13@gmail.com>
2018-05-03 16:47:13 -0700

    Use bls-go to parse data

--- calc.go
+++ calc.go
@@ -15,88 +15,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/clpo13/bls-go"
 )
 
-// Footnote represents arbitrary footnotes sent with the received data.
-type Footnote struct {
-	Code *string `json:",omitempty"`
-	Text *string `json:",omitempty"`
-}
-
-// Change represents calculations of change over 1, 3, 6, and 12 months.
-type Change struct {
-	OneMonth    string `json:"1"`
-	ThreeMonth  string `json:"3"`
-	SixMonth    string `json:"6"`
-	TwelveMonth string `json:"12"`
-}
-
-// Calculation represents calculations sent with the received data.
-type Calculation struct {
-	NetChange Change `json:"net_changes"`
-	PctChange Change `json:"pct_changes"`
-}
-
-// Period represents an individual period (usually a month) of data.
-type Period struct {
-	Year         string      `json:"year"`
-	Num          string      `json:"period"`
-	Name         string      `json:"periodName"`
-	Value        string      `json:"value"`
-	Footnotes    []Footnote  `json:"footnotes"`
-	Calculations *Calculation `json:"calculations,omitempty"`
-}
-
-// Catalog is...
-type Catalog struct {
-	Title	 	 string `json:"series_title"`
-	ID			 string `json:"series_id"`
-	Season	 string `json:"seasonality"`
-	Name		 string `json:"survey_name"`
-	Abbr		 string `json:"survey_abbreviation"`
-	DataType string `json:"measure_data_type"`
-	Area     string `json:"area"`
-	AreaType string `json:"area_type"`
-}
-
-// SeriesData represents data from a single series.
-type SeriesData struct {
-	SeriesID string
-	Catalog  *Catalog  `json:"catalog,omitempty"`
-	Data     []Period
-}
-
-// Series represents a JSON object containing arrays of series.
-type Series struct {
-	Series []SeriesData
-}
-
-// ResultData represents the top-level structure of the received data.
-type ResultData struct {
-	Status       string
-	ResponseTime int
-	Message      []string
-	Results      Series
-}
-
 func main() {
-	f, err := os.Open("testdata2.json")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	if len(os.Args) < 2 {
+		fmt.Println("Please specify a JSON file to read")
+		os.Exit(1)
+  }
 
-	dec := json.NewDecoder(f)
-	var tr ResultData
-	err = dec.Decode(&tr)
+	f, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
+
+	tr := blsgo.ParseData(f)
 
 	catalog := tr.Results.Series[0].Catalog
 	if catalog != nil {
